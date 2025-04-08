d461a7e58814e387091da2fda9ad4ca2fd965f93
divyavikb <166900713+divyavikb@users.noreply.github.com>
2025-04-08 21:07:59 +0530

    Added revision file

---
+++ deploy/scripts/start_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Update the package list
+sudo apt-get update -y
+sudo systemctl start docker
+sudo systemctl enable docker
+# Authenticate Docker to the AWS ECR registry
+
+export DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6
+aws ecr get-login-password --region ap-southeast-2 | \
+sudo docker login --username AWS \
+--password-stdin 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com
+
+
+# Pull the latest Docker image from your ECR repository
+docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
+#Stop the old container (if any) 
+sudo docker stop divyavik_ecr || true 
+sudo docker rm divyavik_ecr || true
+Run the new Docker container
+docker run -d -p 8888:5000 --name my-app15 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6  908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
