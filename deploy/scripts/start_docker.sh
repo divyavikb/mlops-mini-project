f77c876ad12b850bcb5cfd4f51823290939c97e6
divyavikb <166900713+divyavikb@users.noreply.github.com>
2025-04-09 15:26:50 +0530

    Added new CD pipeline2

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -6,19 +6,19 @@ sudo docker login --username AWS \
 --password-stdin 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # Pull the latest image
-sudo docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated2
+sudo docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:v3
 
 # Check if the container 'my-app17' is running
-if [ "$(docker ps -q -f name=my-app17)" ]; then
+if [ "$(docker ps -q -f name=my-app19)" ]; then
     # Stop the running container
-    docker stop my-app17
+    docker stop my-app19
 fi
 
 # Check if the container 'my-app17' exists (stopped or running)
-if [ "$(docker ps -aq -f name=my-app17)" ]; then
+if [ "$(docker ps -aq -f name=my-app19)" ]; then
     # Remove the container if it exists
-    docker rm my-app17
+    docker rm my-app19
 fi
 
 # Run a new container
-docker run -d -p 80:5000 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6 --name my-app17 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated2
+docker run -d -p 80:5000 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6 --name my-app19 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:v3
