c83753476e7dd63bcad5237a778edbe8bbe0e6a9
divyavikb <166900713+divyavikb@users.noreply.github.com>
2025-04-09 10:37:01 +0530

    changes in index  file

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
-# Update the package list
-sudo apt-get update -y
-sudo systemctl start docker
-sudo systemctl enable docker
-# Authenticate Docker to the AWS ECR registry
 
-export DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6
+#!/bin/bash
+# Login to AWS ECR
 aws ecr get-login-password --region ap-southeast-2 | \
 sudo docker login --username AWS \
 --password-stdin 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com
 
+# Pull the latest image
+sudo docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated1
 
-# Pull the latest Docker image from your ECR repository
-sudo docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
-#Stop the old container (if any) 
-sudo docker stop divyavik_ecr || true 
-sudo docker rm divyavik_ecr || true
+# Check if the container 'campusx-app' is running
+if [ "$(docker ps -q -f name=divyavik_ecr)" ]; then
+    # Stop the running container
+    docker stop divyavik_ecr
+fi
 
-docker run -d -p 8888:5000 --name my-app16 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6  908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
+# Check if the container 'campusx-app' exists (stopped or running)
+if [ "$(docker ps -aq -f name=divyavik_ecr)" ]; then
+    # Remove the container if it exists
+    docker rm divyavik_ecr
+fi
+
+# Run a new container
+docker run -d -p 80:5000 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6 --name my-app17 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated1
