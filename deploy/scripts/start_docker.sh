89fe3c7750a7fc328d26cdb04319742526aff865
divyavikb <166900713+divyavikb@users.noreply.github.com>
2025-04-09 09:46:17 +0530

    changes in sh file

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -12,9 +12,9 @@ sudo docker login --username AWS \
 
 
 # Pull the latest Docker image from your ECR repository
-docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
+sudo docker pull 908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
 #Stop the old container (if any) 
 sudo docker stop divyavik_ecr || true 
 sudo docker rm divyavik_ecr || true
-Run the new Docker container
-docker run -d -p 8888:5000 --name my-app15 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6  908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
+
+docker run -d -p 8888:5000 --name my-app16 -e DAGSHUB_PAT=addfe4700403f70eb2e2bc0e6cb76d67a67606e6  908928711788.dkr.ecr.ap-southeast-2.amazonaws.com/divyavik_ecr:vupdated
